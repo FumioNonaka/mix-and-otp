fdbd70aa7bba438457beca6ad3b7de18db786ee6
野中 文雄 <fumio.nonaka@IT-04357-M.local>
2018-10-04 18:55:43 +0900

    MixとOTP 06: ETS

--- kv/lib/kv/registry.ex
+++ kv/lib/kv/registry.ex
@@ -2,56 +2,78 @@ defmodule KV.Registry do
   use GenServer
 
   ## クライアントAPI
-  @doc """
-  登録を始める。
-  """
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, :ok, opts)
-  end
+	@doc """
+	登録を始める。
 
-  @doc """
-  `server`に納められた`name`のプロセスのpidを探す。
+	引数のオプションには`:name`が必須。
+	"""
+	def start_link(opts) do
+		# [1]GenServerのintに名前を渡す。
+		server = Keyword.fetch!(opts, :name)
+		# GenServer.start_link(__MODULE__, :ok, opts)
+		GenServer.start_link(__MODULE__, server, opts)
+	end
 
-  プロセスがあれば`{：ok、pid}`を返し、見つからないときは`：error`を返す。
-  """
-  def lookup(server, name) do
-    GenServer.call(server, {:lookup, name})
-  end
+	@doc """
+	`server`に納められた`name`のプロセスのpidを探す。
+
+	プロセスがあれば`{：ok、pid}`を返し、見つからないときは`：error`を返す。
+	"""
+	def lookup(server, name) do
+		# [2]lookupはサーバーでなく、ETSで直接行う。
+		# GenServer.call(server, {:lookup, name})
+		case :ets.lookup(server, name) do
+			[{^name, pid}] -> {:ok, pid}
+			[] -> :error
+		end
+	end
 
   @doc """
   `server`に`name`の与えられたプロセスがあることを請け合う。
   """
-  def create(server, name) do
-    GenServer.cast(server, {:create, name})
-  end
+	def create(server, name) do
+		# GenServer.cast(server, {:create, name})
+		GenServer.call(server, {:create, name})
+	end
 
   ## サーバーコールバック
-	def init(:ok) do
-		names = %{}
+	# def init(:ok) do
+	def init(table) do
+		# [3]マップnamesをETSテーブルで置き替える。
+		# names = %{}
+		names = :ets.new(table, [:named_table, read_concurrency: true])
 		refs = %{}
 		{:ok, {names, refs}}
 	end
-	
-	def handle_call({:lookup, name}, _from, {names, _} = state) do
-		{:reply, Map.fetch(names, name), state}
-	end
 
-	def handle_cast({:create, name}, {names, refs}) do
-		if Map.has_key?(names, name) do
-			{:noreply, {names, refs}}
-		else
-			# {:ok, pid} = KV.Bucket.start_link([])
-			{:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
-			ref = Process.monitor(pid)
-			refs = Map.put(refs, ref, name)
-			names = Map.put(names, name, pid)
-			{:noreply, {names, refs}}
+	# [4]lookupのコールバックhandle_callは以下に置き替える。
+	# def handle_call({:lookup, name}, _from, {names, _} = state) do
+	# 	{:reply, Map.fetch(names, name), state}
+	# end
+
+	# def handle_cast({:create, name}, {names, refs}) do
+	def handle_call({:create, name}, _from, {names, refs}) do
+		# [5]マップでなくETSテーブルに読み書きする。
+		case lookup(names, name) do
+			# {:ok, _pid} ->
+			{:ok, pid} ->
+				# {:noreply, {names, refs}}
+				{:reply, pid, {names, refs}}
+			:error ->
+				{:ok, pid} = DynamicSupervisor.start_child(KV.BucketSupervisor, KV.Bucket)
+				ref = Process.monitor(pid)
+				refs = Map.put(refs, ref, name)
+				:ets.insert(names, {name, pid})
+				# {:noreply, {names, refs}}
+				{:reply, pid, {names, refs}}
 		end
 	end
 
 	def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+		# [6]マップでなくETSテーブルから削除する。
 		{name, refs} = Map.pop(refs, ref)
-		names = Map.delete(names, name)
+		# names = Map.delete(names, name)
+		:ets.delete(names, name)
 		{:noreply, {names, refs}}
 	end
 
--- kv/test/kv/registry_test.exs
+++ kv/test/kv/registry_test.exs
@@ -1,9 +1,12 @@
 defmodule KV.RegistryTest do
   use ExUnit.Case, async: true
 
-  setup do
-    registry = start_supervised!(KV.Registry)
-    %{registry: registry}
+  # setup do
+  setup context do
+    # registry = start_supervised!(KV.Registry)
+    _ = start_supervised!({KV.Registry, name: context.test})
+    # %{registry: registry}
+    %{registry: context.test}
   end
 
   test "spawns buckets", %{registry: registry} do
@@ -16,19 +19,22 @@ defmodule KV.RegistryTest do
     assert KV.Bucket.get(bucket, "milk") == 1
 	end
 
-	test "removes buckets on exit", %{registry: registry} do
-		KV.Registry.create(registry, "shopping")
-		{:ok, bucket} = KV.Registry.lookup(registry, "shopping")
-		Agent.stop(bucket)
-		assert KV.Registry.lookup(registry, "shopping") == :error
+  test "removes buckets on exit", %{registry: registry} do
+    KV.Registry.create(registry, "shopping")
+    {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+    # 登録プロセスに:DOWNメッセージを遅らせるための呼び出し
+    _ = KV.Registry.create(registry, "bogus")
+    assert KV.Registry.lookup(registry, "shopping") == :error
   end
 
   test "removes bucket on crash", %{registry: registry} do
     KV.Registry.create(registry, "shopping")
     {:ok, bucket} = KV.Registry.lookup(registry, "shopping")
-  
     # プロセスを正常でない理由で止める
     Agent.stop(bucket, :shutdown)
+    # 登録プロセスに:DOWNメッセージを遅らせるための呼び出し
+    _ = KV.Registry.create(registry, "bogus")
     assert KV.Registry.lookup(registry, "shopping") == :error
-  end  
+  end
 end
